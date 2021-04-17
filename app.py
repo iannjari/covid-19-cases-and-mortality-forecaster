b41927d3c4a4758d070e8611ccf37674bf8551dc
Ian Njari <iannjari@gmail.com>
2021-04-18 00:13:41 +0300

    Add Plotly Dash app, update notebook and map data

--- app.py
+++ app.py
@@ -4,25 +4,50 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go 
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
+import os
+
+pwd=os.getcwd()
+df1=pd.read_excel(pwd+"\\cases.xlsx")
 
 app = dash.Dash(__name__)
+fig = go.Figure()
 
 app.layout = html.Div([
-    dcc.Graph(id="graph"),
-    html.Button("Switch Axis", id='btn', n_clicks=0)
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in df1['Country/Region'].unique()],
+        value='Kenya'),
+    
+    dcc.Graph(figure=fig),
+    
+    html.Div(id='dd-output-container')
 ])
 
 @app.callback(
-    Output("graph", "figure"), 
-    [Input("btn", "n_clicks")])
-def display_graph(n_clicks):
-    if n_clicks % 2 == 0:
-        x, y = 'AAPL_x', 'AAPL_y'
-    else:
-        x, y = 'AAPL_y', 'AAPL_x'
+    Output("graph", "fig"), 
+    [Input("dropdown", "value")])
 
-    fig = px.line(df, x=x, y=y)    
+
+def get_dff1(df1):
+    dff1 = df1.loc[df1['Country/Region']== 'value']
+    return dff1
+
+def get_y(dff1):
+    
+    dff1.columns = range(dff1.shape[1])
+    dff2=pd.Index(dff1.iloc[0])
+    yval=dff2[1:]
+    return yval
+
+def display_graph(value,dff1,yval):
+   
+    
+    fig = px.line(dff1, x=dff1.columns[1:], y=yval)    
     return fig
-app.run_server(debug=True)
+
+
+    
+    
+app.run_server(debug=True) 
