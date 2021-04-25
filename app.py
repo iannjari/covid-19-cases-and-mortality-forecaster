bd74e2b34d3c770ee8b93fbb27a75c7761c7c4c4
Ian Njari <iannjari@gmail.com>
2021-04-25 18:30:55 +0300

    App callback finally works!

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import os
 
 pwd=os.getcwd()
 df1=pd.read_excel(pwd+"\\cases.xlsx")
+df2=pd.read_excel(pwd+"\\cases_plot.xlsx")
 
 app = dash.Dash(__name__)
 fig = go.Figure()
@@ -17,36 +18,35 @@ fig = go.Figure()
 app.layout = html.Div([
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in df1['Country/Region'].unique()],
-        value='Kenya'),
+        value = 'Afghanistan',
+        options=[{'label': i, 'value': i} for i in df2.columns[1:]]),
     
-    dcc.Graph(figure=fig),
+    dcc.Graph(id='graph'),
     
     html.Div(id='dd-output-container')
 ])
 
 @app.callback(
-    Output("graph", "fig"), 
-    [Input("dropdown", "value")])
+    Output("graph", "figure"), 
+    [Input('dropdown', 'value')])
 
 
-def get_dff1(df1):
-    dff1 = df1.loc[df1['Country/Region']== 'value']
-    return dff1
-
-def get_y(dff1):
+def display_graph(dropdown):
     
-    dff1.columns = range(dff1.shape[1])
-    dff2=pd.Index(dff1.iloc[0])
-    yval=dff2[1:]
-    return yval
-
-def display_graph(value,dff1,yval):
-   
+    dffd=df1.loc[df1['Country/Region'] == dropdown]
+    dffd=dffd.reset_index()
+    dffd=dffd.rename_axis(None, axis=1)
+    dffd=dffd.drop(['index','Country/Region'],axis=1)
     
-    fig = px.line(dff1, x=dff1.columns[1:], y=yval)    
+    
+    fig = px.line(df2,x=df2["Date"], y=dffd.loc[0],
+                  hover_data={"Date"},
+                  title='Cases',
+                  labels={"y": "No. of Cases"}
+                  )
     return fig
 
+ 
 
     
     
